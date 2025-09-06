028747bf2418f3cc4fc450c3a362c85741a145ac
Юлия Александрова <julialeks96@gmail.com>
2025-09-06 08:10:24 +0300

    add initial commit

---
+++ guess_number.py
@@ -0,0 +1,18 @@
+import time
+
+
+def values():
+    for value in range(5):
+        print(value)
+        if value%2 == 0:
+            print('Четное')
+        else:
+            print('Нечетное')
+        print('Конец итерации') 
+
+values()# Комментарии
+
+STROKA = 'Check this lifehack'
+for c in STROKA:
+    print(c, end='', flush=True)
+    time.sleep(0.1)
